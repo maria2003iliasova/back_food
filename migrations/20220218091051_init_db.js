251e18568376c13adbed0e32140c98fb65b59972
maria2003iliasova <mia03.03@mail.ru>
2022-02-25 12:25:18 +0300

    update

--- migrations/20220218091051_init_db.js
+++ migrations/20220218091051_init_db.js
@@ -5,7 +5,7 @@ exports.up = async function(knex) {
         table.string("email").notNullable();
         table.boolean("email_is_confirmed").notNullable().defaultTo(false);
         table.string("email_confirmation_code", 6);
-        table.string("password");
+        table.string("password").notNullable();
         table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
         table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
         table.enu("role", ["user", "admin"]).notNullable().defaultTo("user");
@@ -33,7 +33,7 @@ exports.up = async function(knex) {
         table.text("text").notNullable();
         table.integer("author_id").notNullable();
         table.enu("evaluation", [1, 2, 3, 4, 5]).notNullable();
-        table.boolean("published").notNullable().defaultTo(false);
+        table.boolean("published").notNullable().defaultTo(true);
         table.integer("order_id").notNullable();
         table.integer("photo_gallery_id").notNullable();
         table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
@@ -85,11 +85,11 @@ exports.up = async function(knex) {
 };
 
 exports.down = async function(knex) {
-    await knex.schema.dropTableIfExists("users");
-    await knex.schema.dropTableIfExists("orders");
-    await knex.schema.dropTableIfExists("reviews");
+    await knex.schema.dropTableIfExists("orders_goods");
     await knex.schema.dropTableIfExists("goods");
-    await knex.schema.dropTableIfExists("photo_galleries");
     await knex.schema.dropTableIfExists("photos");
-    await knex.schema.dropTableIfExists("order_goods");
+    await knex.schema.dropTableIfExists("reviews");
+    await knex.schema.dropTableIfExists("orders");
+    await knex.schema.dropTableIfExists("photo_galleries");
+    await knex.schema.dropTableIfExists("users");
 };
