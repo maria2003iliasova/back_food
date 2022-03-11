105174e46d8167ef80c03ae3f488bbc973a05dd0
maria2003iliasova <mia03.03@mail.ru>
2022-03-11 11:33:41 +0300

    Add tests

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,36 @@
 require('dotenv').config();
 
 module.exports = {
-    client: 'pg',
-    connection: { 
-        host : process.env.DB_HOST,
-        port : process.env.DB_PORT,
-        user : process.env.DB_USERNAME,
-        password : process.env.DB_PASSWORD,
-        database : process.env.DB_DATABASE
+    development: {
+        client: "pg",
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        },
+        migrations: {
+            tableName: "migrations",
+        },
+        seeds: {
+            directory: "./seeds",
+        },
     },
-    migrations: {
-        tableName: 'migrations'
+    test: {
+        client: "pg",
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        },
+        migrations: {
+            tableName: "migrations",
+        },
+        seeds: {
+            directory: "./seeds",
+        },
     },
-    seeds: {
-        directory: "./seeds"
-    }
 };
